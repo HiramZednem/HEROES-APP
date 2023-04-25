21fcceb5acac91c3e702546fa0aa1bec7ab6a09b
Hiram <hculebro@neodata.com.mx>
2023-04-24 21:12:17 -0600

    img added and description per hero

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient  } from '@angular/common/http';
 import { Hero } from '../interface/hero.interface';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 import { environments } from '../../../environments/environments';
 
 @Injectable({
@@ -15,5 +15,15 @@ export class HeroesService {
 
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${ this.baseUrl }/heroes`);
+  }
+
+  //aqui cree un nuevo metodo para poder obtener el heroe que necesito.
+  getHeroById( id: string ): Observable<Hero|undefined> {
+    return this.http.get<Hero>(`${this.baseUrl}/heroes/${ id }`)
+      .pipe(
+        //para manejar el error, manejo la data con un pipe, si el pipe me dice, hey carnal hay un error cae en la funcion de abajo
+        //una vez aca, ejecuta lo que tengamos por aca, en este caso of es de rxjs y regresa un observable
+        catchError( error => of(undefined) )
+      );
   }
 }
