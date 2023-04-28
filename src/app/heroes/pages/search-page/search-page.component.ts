e7bb3427395ca354281d37ab633da518f9782dcd
Hiram <hculebro@neodata.com.mx>
2023-04-27 19:30:15 -0600

    quick fix for search page

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -3,6 +3,7 @@ import { FormControl } from '@angular/forms';
 import { Hero } from '../../interface/hero.interface';
 import { HeroesService } from '../../services/heroes.service';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-page',
@@ -15,7 +16,10 @@ export class SearchPageComponent {
   public heroes: Hero[] = []
   public selectedHero?: Hero;
 
-  constructor( private heroService: HeroesService) {}
+  constructor(
+    private heroService: HeroesService,
+    private router: Router
+  ) {}
 
   searchHero() {
     const value: string = this.searchInput.value || '';
@@ -30,5 +34,7 @@ export class SearchPageComponent {
     const hero: Hero = event.option.value;
     this.searchInput.setValue( hero.superhero );
     this.selectedHero = hero;
+    this.router.navigate(['/heroes', hero.id])
+
   }
 }
